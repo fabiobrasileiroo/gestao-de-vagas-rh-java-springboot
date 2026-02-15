d7efeefcd7afbbedd3835b6119626f9cd3109fc6
fabiobrasileiroo <fabio.h591@gmail.com>
2026-02-15 15:00:59 -0400

    :sparkles: feat: adicionar entidade Candidate com campos e anotações JPA; atualizar configuração do application.yaml

--- src/main/java/com/br/fabiobrasileiro/gestao_vagas/main/GestaoVagasApplication.java
+++ src/main/java/com/br/fabiobrasileiro/gestao_vagas/main/GestaoVagasApplication.java
@@ -2,10 +2,12 @@ package com.br.fabiobrasileiro.gestao_vagas.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.br.fabiobrasileiro")
+@EntityScan(basePackages = "com.br.fabiobrasileiro.gestao_vagas.modules")
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/br/fabiobrasileiro/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/br/fabiobrasileiro/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,19 +1,27 @@
 package com.br.fabiobrasileiro.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
+@Table(name = "candidate")
 public class CandidateEntity {
-  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
 
@@ -29,5 +37,8 @@ public class CandidateEntity {
 
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createAt;
 
 }
