647da7d2490651560fccc871fa2b46609bdb32bf
fabiobrasileiroo <fabio.h591@gmail.com>
2026-02-15 16:33:39 -0400

    :sparkles: feat: reestruturar aplicação e adicionar repositório Candidate com persistência no bd

--- src/main/java/com/br/fabiobrasileiro/GestaoVagasApplication.java
+++ src/main/java/com/br/fabiobrasileiro/GestaoVagasApplication.java
@@ -1,13 +1,15 @@
-package com.br.fabiobrasileiro.gestao_vagas.main;
+package com.br.fabiobrasileiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.br.fabiobrasileiro")
-@EntityScan(basePackages = "com.br.fabiobrasileiro.gestao_vagas.modules")
+// @ComponentScan(basePackages = "com.br.fabiobrasileiro")
+// @EntityScan(basePackages = "com.br.fabiobrasileiro.gestao_vagas.modules")
+// @EnableJpaRepositories(basePackages = "com.br.fabiobrasileiro.gestao_vagas.modules")
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/br/fabiobrasileiro/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/br/fabiobrasileiro/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,8 +1,9 @@
 package com.br.fabiobrasileiro.gestao_vagas.modules.candidate.controllers;
 
 import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.fabiobrasileiro.gestao_vagas.modules.candidate.CandidateEntity;
+import com.br.fabiobrasileiro.gestao_vagas.modules.candidate.CandidateRepository;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-  @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @PostMapping(value = "/")
   public ResponseEntity<CandidateEntity> create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    // Aqui você pode persistir o candidato ou processar como necessário.
-    // Retornamos o objeto recebido com status 201 (Created) para o cliente.
-    return ResponseEntity.status(HttpStatus.CREATED).body(candidateEntity);
+    var value = this.candidateRepository.save(candidateEntity);
+    return ResponseEntity.status(HttpStatus.CREATED).body(value);
   }
 }
